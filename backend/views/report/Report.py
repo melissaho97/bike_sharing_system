6ca09733b1600137982e2c2e396289785cd51476
Melissa Ho <32935782+melissaho97@users.noreply.github.com>
2019-11-05 12:19:59 +0000

    update

--- backend/views/report/Report.py
+++ backend/views/report/Report.py
@@ -11,7 +11,9 @@ from tkinter import *
 from tkinter import ttk
 import pandas as pd
 import pymysql
+# Provides classic Python interface
 from matplotlib import pyplot as plt
+import matplotlib.dates as mdates  # Handle Dates
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 def init_styleSheet():
@@ -80,26 +82,74 @@ class ReportMngPage(tk.Frame):
         h_frame = tk.Frame(self, bg = styleDict["TabHeaderBgColor"], height = styleDict["topPadding"])
         h_frame.pack(fill = tk.X, padx = styleDict["xPadding"], pady = styleDict["yPadding"])
         
+        # Select City Name Frame
+        city_name_frame = tk.Frame(self)
+        city_name_frame.pack(fill = tk.X, padx = styleDict["xPadding"], pady = styleDict["yPadding"])
+        city_name_label = tk.Label(city_name_frame, text = "City Name: ", width = styleDict["labelLen"], anchor = tk.W)
+        city_name_label.pack(side = tk.LEFT)
+        
+        #Get City Name from DB
+        connection = connectDB()
+        cursor = connection.cursor()
+        query = '''SELECT City_Name `City Name` FROM city;'''
+        cursor.execute(query)
+        city_list = []
+        for row in cursor.fetchall():
+            city_list.append(row)
+        disconnectDB(connection)
+        
+        self.var_city_name = StringVar()
+        city_name_input = ttk.Combobox(city_name_frame, values = city_list, state='readonly', textvariable = self.var_city_name)
+        city_name_input.set('Select City')
+        city_name_input.pack(fill = tk.X)
+        
         # Import Data from Transaction Table
         
-        # X-Axis: City Names
+        # X-Axis Label: Months
+# =============================================================================
+#         x_label = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+#         
+#         # Y-Axis Label: Total Sales Amount per Month
+#         connection = connectDB()
+#         cursor = connection.cursor()
+#         query = '''SELECT Paid_Amount FROM transaction WHERE Updated_At;'''
+#         cursor.execute(query)
+#         city_list = []
+#         for row in cursor.fetchall():
+#             city_list.append(row)
+#         disconnectDB(connection)
+# =============================================================================
         
-        # Y-Axis: Amount 
-        Data = {
-        'Country': ['US','CA','GER','UK','FR'],
-        'GDP_Per_Capita': [45000,42000,52000,49000,47000]
+        
+        Data1 = {
+        'Time': ['US','CA','GER','UK','FR'],
+        'Sales_Amount': [45000,42000,52000,49000,47000]
        }
-        
-        
-        df1 = pd.DataFrame(Data, columns= ['Country', 'GDP_Per_Capita'])
-        df1 = df1[['Country', 'GDP_Per_Capita']].groupby('Country').sum()
-        print (df1)
-        
-        figure1 = plt.Figure(figsize=(6,5), dpi=100)
-        ax1 = figure1.add_subplot(111)
-        bar1 = FigureCanvasTkAgg(figure1, master)
-        bar1.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
-        df1.plot(kind='bar', legend=True, ax=ax1)
-        ax1.set_title('Country Vs. GDP Per Capita')
 
+        # Create Pandas to structure the input data
+        df1 = pd.DataFrame(Data1, columns= ['Time', 'Sales_Amount'])
+        df1 = df1[['Time', 'Sales_Amount']].groupby('Time').sum()
+        
+        # Create a figure object
+        # Figure params: figsize(width, height), 
+        figure1 = plt.Figure(figsize=(6,5), dpi=110, facecolor="b")
+        # add_subplot ->> add multiple plots to a figure
+        ax1 = figure1.add_subplot(111)
+        
+        # FigureCanvasTkAgg can then generate a widget for Tkinter to use
+        bar1 = FigureCanvasTkAgg(figure1, master)
+        
+        bar1.get_tk_widget().pack(fill=tk.BOTH)
+        df1.plot(kind='line', legend=True, ax=ax1)
+        plt.title(figure1, 'Sales Report')
+        
+        #Set Action Buttion Frame
+        act_button_frame = tk.Frame(self)
+        act_button_frame.pack(padx = styleDict["xPadding"], pady = styleDict["yPadding"])
+        save_button = tk.Button(act_button_frame, text = "Save", width = styleDict["buttonWidth"], 
+                                command = self.saveImg)
+        save_button.pack(side = tk.LEFT)
+        
+    def saveImg(self):
+        plt.savefig('my_figure.png')
         

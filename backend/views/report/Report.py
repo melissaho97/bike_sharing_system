3af589de623dc561d4eeade33a8bc603a6dab11d
Melissa Ho <32935782+melissaho97@users.noreply.github.com>
2019-11-06 08:22:21 +0000

    update

--- backend/views/report/Report.py
+++ backend/views/report/Report.py
@@ -18,6 +18,7 @@ import matplotlib.dates as mdates
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import numpy as np
+import array
 
 def init_styleSheet():
     global styleDict 
@@ -107,63 +108,96 @@ class ReportMngPage(tk.Frame):
         self.var_city_name = tk.StringVar()
         city_name_input = ttk.Combobox(city_name_frame, values = city_list, state='readonly', textvariable = self.var_city_name)
         city_name_input.bind("<<ComboboxSelected>>", self.callback)
-        city_name_input.insert(0, 'Select City')
+        #city_name_input.insert(0, 'Select City')
         city_name_input.pack(fill = tk.X)
         
+        act_button_frame = tk.Frame(self)
+        act_button_frame.pack(padx = styleDict["xPadding"], pady = styleDict["yPadding"])
+        save_button = tk.Button(act_button_frame, text = "Save", width = styleDict["buttonWidth"], command = self.saveImg)
+        save_button.pack(side = tk.RIGHT, fill = tk.X, padx = styleDict["inlinePadding"])
+        
+        # Initialise 2 arrays
+        self.arr_x = []
+        self.arr_y = []
+        print('Store Database')
+        
+        # Changing List into NumPy Array
+        x = np.array(self.arr_x)
+        y = np.array(self.arr_y)
+        
+        # Plot
+        plt.plot(self.arr_x, self.arr_y)
+        plt.xlabel("Time")
+        plt.ylabel("Sales")
+        plt.show()
+        
+# =============================================================================
+#         fig = plt.Figure(figsize=(len(self.arr_x),len(self.arr_y)))
+#         a = fig.add_subplot(111)
+#         a.plot(x, y, color='red')
+#          
+#         a.set_title ("Sales Report", fontsize=16)
+#         a.set_ylabel("Sales", fontsize=14)
+#         a.set_xlabel("Time", fontsize=14)
+#          
+#         canvas = FigureCanvasTkAgg(fig, master)
+#         canvas.get_tk_widget().pack()
+#         canvas.draw()
+# =============================================================================
+        
+# =============================================================================
+#         Data1 = {
+#         'Time': ['US','CA','GER','UK','FR'],
+#         'Sales_Amount': [45000,42000,52000,49000,47000]
+#        }
+#         
+#         # Create Pandas to structure the input data
+#         df1 = pd.DataFrame(Data1, columns= ['Time', 'Sales_Amount'])
+#         df1 = df1[['Time', 'Sales_Amount']].groupby('Time').sum()
+#         
+#         # Create a figure object
+#         # Figure params: figsize(width, height), 
+#         figure1 = plt.Figure(figsize=(6,5), dpi=110, facecolor="b")
+#         # add_subplot ->> add multiple plots to a figure
+#         ax1 = figure1.add_subplot(111)
+#         
+#         # FigureCanvasTkAgg can then generate a widget for Tkinter to use
+#         bar1 = FigureCanvasTkAgg(figure1, master)
+#         bar1.get_tk_widget().pack(fill=tk.BOTH)
+#         df1.plot(kind='line', legend=True, ax=ax1)
+#         
+#         #Set Action Buttion Frame
+#         act_button_frame = tk.Frame(self)
+#         act_button_frame.pack(padx = styleDict["xPadding"], pady = styleDict["yPadding"])
+# =============================================================================
         
     def callback(self, event):
-        self.setCurrentLocData()
-        
-    def setCurrentLocData(self):
-        print(self.var_city_name.get())
-        print(self.var_status.get())
-        # Retrieve Data
         connection = connectDB()
         print("Database")
         cursor = connection.cursor()
         print("Cursor")
-        cursor.execute('''SELECT DATE_FORMAT(trans.Updated_At,'%%%%Y-%%%%m') AS Date, trans.Status FROM transaction AS trans WHERE trans.Status=%s;''', (self.var_status.get()))
-        #query = "SELECT CAST(SUM(trans.`Paid_Amount`) AS CHAR) AS Amount, trans.Status FROM transaction AS trans WHERE trans.Status='%s';"
-        
-# =============================================================================
-#         query='''SELECT CAST(SUM(trans.`Paid_Amount`) AS CHAR) AS Amount, DATE_FORMAT(trans.Updated_At,'%%%%Y-%%%%m') AS Date, 
-#                     trans.Origin_ID, trans.Status,
-#     				l.ID, l.City_ID, 
-#     				c.ID, c.City_Name
-#                     FROM transaction AS trans
-#     				INNER JOIN location AS l ON trans.Origin_ID=l.ID
-#     				INNER JOIN city AS c ON l.City_ID=c.ID
-#     				WHERE c.City_Name=%s AND trans.Status=%s
-#                     GROUP BY Date, trans.Origin_ID, l.City_ID
-#     				ORDER BY Date;'''
-# =============================================================================
-# =============================================================================
-#         query = '''SELECT SUM(trans.Paid_Amount) AS Amount, DATE_FORMAT(trans.Updated_At,'%y-%m') AS Date, 
-#                     trans.Origin_ID, trans.Status,
-#     				l.ID, l.City_ID, 
-#     				c.ID, c.City_Name
-#                     FROM transaction AS trans
-#     				INNER JOIN location AS l ON trans.Origin_ID=l.ID
-#     				INNER JOIN city AS c ON l.City_ID=c.ID
-#     				WHERE c.City_Name=%s AND trans.Status=%s
-#                     GROUP BY DATE_FORMAT(trans.Updated_At,'%y-%m'), trans.Origin_ID, l.City_ID
-#     				ORDER BY DATE_FORMAT(trans.Updated_At,'%y-%m');'''
-# =============================================================================
-        #query_params = (self.var_city_name.get(),self.var_status.get())
-        #print(cursor.execute(query, (self.var_city_name.get(),self.var_status.get())))
-        
-        # Initialise 2 arrays
-        #arr_x = []
-        arr_y = []
+        query = '''SELECT CAST(SUM(trans.`Paid_Amount`) AS CHAR) AS Amount, DATE_FORMAT(trans.Updated_At,"%%Y-%%m") AS Date, 
+                    trans.Origin_ID, trans.Status,
+    				l.ID, l.City_ID, 
+    				c.ID, c.City_Name
+                    FROM transaction AS trans
+    				INNER JOIN location AS l ON trans.Origin_ID=l.ID
+    				INNER JOIN city AS c ON l.City_ID=c.ID
+    				WHERE c.City_Name=%s AND trans.Status=%s
+                    GROUP BY Date, trans.Origin_ID, l.City_ID
+    				ORDER BY Date;'''
+        query_params = (self.var_city_name.get(), self.var_status.get())
+        print(cursor.execute(query, query_params))
         
         # Store Data
         for row in cursor.fetchall():
-            #arr_x.append(row[1])
-            arr_y.append(row[0])
+            arr_x.set.append(row[1])
+            arr_y.set.append(row[0])
         disconnectDB(connection)
         
-        #print(arr_x)
+        print(arr_x)
         print(arr_y)
+        
 # =============================================================================
 #         x = np.array(arr_x)
 #         y = np.array(arr_y)
@@ -182,9 +216,8 @@ class ReportMngPage(tk.Frame):
 #         canvas = FigureCanvasTkAgg(fig, master=self.window)
 #         canvas.get_tk_widget().pack()
 #         canvas.draw()
-# 
-#         
-#     def saveImg(self):
-#         plt.savefig('my_figure.png')
 # =============================================================================
+
+    def saveImg(self):
+        plt.savefig('my_figure.png')
         
--- backend/views/report/test.py
+++ backend/views/report/test.py
@@ -23,37 +23,33 @@ Created on Tue Nov  5 15:29:29 2019
 # =============================================================================
         
                 
-# =============================================================================
-#         Data1 = {
-#         'Time': ['US','CA','GER','UK','FR'],
-#         'Sales_Amount': [45000,42000,52000,49000,47000]
-#        }
-# 
-#         # Create Pandas to structure the input data
-#         df1 = pd.DataFrame(Data1, columns= ['Time', 'Sales_Amount'])
-#         df1 = df1[['Time', 'Sales_Amount']].groupby('Time').sum()
-# =============================================================================
+        Data1 = {
+        'Time': ['US','CA','GER','UK','FR'],
+        'Sales_Amount': [45000,42000,52000,49000,47000]
+       }
+
+        # Create Pandas to structure the input data
+        df1 = pd.DataFrame(Data1, columns= ['Time', 'Sales_Amount'])
+        df1 = df1[['Time', 'Sales_Amount']].groupby('Time').sum()
         
-# =============================================================================
-#         # Create a figure object
-#         # Figure params: figsize(width, height), 
-#         figure1 = plt.Figure(figsize=(6,5), dpi=110, facecolor="b")
-#         # add_subplot ->> add multiple plots to a figure
-#         ax1 = figure1.add_subplot(111)
-#         
-#         # FigureCanvasTkAgg can then generate a widget for Tkinter to use
-#         bar1 = FigureCanvasTkAgg(figure1, master)
-#         bar1.get_tk_widget().pack(fill=tk.BOTH)
-#         df1.plot(kind='line', legend=True, ax=ax1)
-#         plt.title(figure1, 'Sales Report')
-#         
-#         #Set Action Buttion Frame
-#         act_button_frame = tk.Frame(self)
-#         act_button_frame.pack(padx = styleDict["xPadding"], pady = styleDict["yPadding"])
-#         save_button = tk.Button(act_button_frame, text = "Save", width = styleDict["buttonWidth"], 
-#                                 command = self.saveImg)
-#         save_button.pack(side = tk.LEFT)
-# =============================================================================
+        # Create a figure object
+        # Figure params: figsize(width, height), 
+        figure1 = plt.Figure(figsize=(6,5), dpi=110, facecolor="b")
+        # add_subplot ->> add multiple plots to a figure
+        ax1 = figure1.add_subplot(111)
+        
+        # FigureCanvasTkAgg can then generate a widget for Tkinter to use
+        bar1 = FigureCanvasTkAgg(figure1, master)
+        bar1.get_tk_widget().pack(fill=tk.BOTH)
+        df1.plot(kind='line', legend=True, ax=ax1)
+        plt.title(figure1, 'Sales Report')
+        
+        #Set Action Buttion Frame
+        act_button_frame = tk.Frame(self)
+        act_button_frame.pack(padx = styleDict["xPadding"], pady = styleDict["yPadding"])
+        save_button = tk.Button(act_button_frame, text = "Save", width = styleDict["buttonWidth"], 
+                                command = self.saveImg)
+        save_button.pack(side = tk.LEFT)
         
         
         
